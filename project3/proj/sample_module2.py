e62239e108a93c641be30dd06f848d691398912f
arvind bora <sonu06sagar@gmail.com>
2020-04-11 12:07:32 +0530

    unittest Skipping methods

---
+++ project3/proj/sample_module2.py
@@ -0,0 +1,18 @@
+def add2num(x, y):
+    """Adds two given numbers and returns the sum.
+    >>> add2num(6, 7)
+    13
+    >>> add2num(-8.5, 7)
+    -2.5
+    """
+    return x + y
+
+
+def pow2num(x, y):
+    """Returns x power of y.
+    >>> pow2num(2, 6)
+    64
+    >>> pow2num(10, -3)
+    0.001
+    """
+    return x**y
